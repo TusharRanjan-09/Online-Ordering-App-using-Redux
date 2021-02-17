147fb154074beabf0de1556dca7431ee7b095076
TusharRanjan-09 <tushar.r@jeanmartin.com>
2021-02-17 18:27:05 +0530

    Updated

--- App.js
+++ App.js
@@ -7,23 +7,22 @@ import HomeScreen from './src/screens/Home';
 import BooksScreen from './src/screens/Books';
 import ElectronicsScreen from './src/screens/Electronics';
 import CartScreen from './src/components/CartScreen';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import store from './src/store/index';
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <Provider store={store}>
-       <NavigationContainer>
-      <Stack.Navigator headerMode={false}>
-      <Stack.Screen name="Shopping Cart" component={HomeScreen}/>
-        <Stack.Screen name="BooksScreen" component={BooksScreen} />     
-        <Stack.Screen name="ElectronicsScreen" component={ElectronicsScreen} />
-        <Stack.Screen name="Cart" component={CartScreen} />
-
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator headerMode={false}>
+          <Stack.Screen name="Shopping Cart" component={HomeScreen} />
+          <Stack.Screen name="BooksScreen" component={BooksScreen} />
+          <Stack.Screen name="ElectronicsScreen" component={ElectronicsScreen} />
+          <Stack.Screen name="Cart" component={CartScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </Provider>
-   
+
   );
 }
 const styles = StyleSheet.create({
