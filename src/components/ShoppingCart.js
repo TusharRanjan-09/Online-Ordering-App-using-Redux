de9a25fdc34e450d3a93e0e7b3ec325874d7934f
TusharRanjan-09 <tushar.r@jeanmartin.com>
2021-02-17 18:27:58 +0530

    Updated

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -17,12 +17,10 @@ function ShoppingCart(props) {
                 <Text style={{ fontSize: 24, color: 'white', marginLeft: 135 }}>{props.header}</Text>
             </View>
             <View style={{ flexDirection: 'row', marginLeft: 100 }}>
-                <TouchableOpacity onPress={()=> navigation.navigate('Cart')}>
-                    {/* <Text>Press Me</Text> */}
-                    <FontAwesome5 name="shopping-cart" size={28} color="white"/>
+                <TouchableOpacity onPress={() => navigation.navigate('Cart')}>
+                    <FontAwesome5 name="shopping-cart" size={28} color="white" />
                 </TouchableOpacity>
-
-                <View style={{ backgroundColor: 'yellow', opacity: .75, width: 20, height: 20, borderRadius: 20, justifyContent: 'center', alignContent: 'center',  zIndex: 2000 }}>
+                <View style={{ backgroundColor: 'yellow', opacity: .75, width: 20, height: 20, borderRadius: 20, justifyContent: 'center', alignContent: 'center', zIndex: 2000 }}>
                     <Text style={{ color: 'red', position: 'absolute', fontSize: 22, fontWeight: 'bold', padding: 3.5, }}>{props.cartItems.length}</Text>
                 </View>
             </View>
